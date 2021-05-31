4ed918cff47e4dd6d9a047a5b32b71802040c944
refikbakalim <refikbakalim@gmail.com>
2021-05-31 04:36:47 +0300

    Update lynarx.py
    Cleaned up variable names and added prefix function and upgraded aram function

--- lynarx.py
+++ lynarx.py
@@ -1,143 +1,237 @@
+from asyncio.windows_events import NULL
 import discord
-from random import randint
-import asyncio
+import random
 import time
 import datetime
 import os
 import sys
 
-def read_token():
+def read_token(): #reads discord bot token from the txt
     with open("token.txt", "r") as f:
         lines = f.readlines()
         return lines[0].strip()
 
+def read_prefix(): #reads the prefix it saved before
+    with open("prefix.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
 token = read_token()
+
 client = discord.Client()
-messages = joined = player = aram = 0
+
+aram_in_progress = False #initialize global variables
+bot_prefix = read_prefix()
+
 
 @client.event
-async def on_connect():
+async def on_connect(): #when connected to server print a message
     print("Connected to discord")
 
+
 @client.event
-async def on_ready():
+async def on_ready(): #when ready to operate print the status
     print(f'Logged in as {client.user.name} (ID: {client.user.id}) (Latency: {int(client.latency*1000)}ms)')
     print("Ready")
     print('------')
 
+
 @client.event
-async def on_message(message):
+async def on_message(message): #when a message comes
 
     if message.author == client.user: #if the message comes from this bot, do nothing
         return
 
-    global messages, player, aram
+
+    global aram_in_progress #define global variables
+    global bot_prefix
+
    
-    messages += 1
-    allowed_channels = ["genel", "bottest", "bot"]
+    allowed_channels = ["genel", "bottest", "bot", "aram"]
+
 
     if str(message.channel) in allowed_channels or not message.guild: # only work in allowed_channels or in private message
 
-        if '<a:ratJAM:823610543520612364>' in message.content: # if ratJam exists in a message, add as a reaction
+        if '<a:ratJAM:823610543520612364>' in message.content: # if ratJam exists in the message, add its emote as a reaction
             await message.add_reaction('<a:ratJAM:823610543520612364>')
 
-        if not message.guild: # on direct message taken, log it
+
+        if not message.guild: #on direct message taken, send an answer and log the message
             await message.author.send("I don't answer to private messages")
             with open("dms.txt", "a", encoding="utf-8") as f:
                 f.write(f"Date:{datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')} {message.author} : {message.content}  \n")
 
-        elif message.content.startswith('$delete'): # deletes messages, 50 max in 1 run
-            if(message.author.id == 181439459894624256):
-                number = int(message.content.split()[1])
-                if number > 50:
-                    number = 50
-                try:
-                    await message.channel.purge(limit=number+1)
-                except:
-                    await message.channel.send("Write a proper integer Ex. $delete 5 " + message.author.mention, mention_author=True)
+        elif message.content.startswith(bot_prefix + "help"): # shows help menu
+            bot_channel = client.get_channel(594596796890873871) # gets the id of "bot" channel
+            aram_channel = client.get_channel(848731423645106217) # gets the id of "aram" channel
+            embed = discord.Embed(title = "Commands", description = "Description of the commands")
+            embed.add_field(name = bot_prefix + "delete", value = "Deletes messages")
+            embed.add_field(name = bot_prefix + "dice", value = "Returns random number between 1 and 6")
+            embed.add_field(name = bot_prefix + "aram", value = f'only works in {bot_channel.mention} and {aram_channel.mention}')
+            embed.add_field(name = bot_prefix + "restart", value = "Restarts the bot")
+            embed.add_field(name = bot_prefix + "exit", value = "Disconnects the bot")
+            embed.add_field(name = bot_prefix + "prefix", value = f"Changes the prefix, current \"{bot_prefix}\"")
+            await message.channel.send(content=None, embed=embed)
+
+        elif message.content.startswith(bot_prefix + "prefix"): #changes the prefix of bot default "!"
+            try:
+                if(len(message.content.split()) == 1):
+                    bot_prefix = "!"
+                else:
+                    bot_prefix = message.content.split()[1]
+
+                with open('prefix.txt', "w") as file:
+                        file.write(bot_prefix)
+                await message.channel.send(f"Changed prefix to \"{bot_prefix}\"")
+
+            except:
+                await message.channel.send(f"Wrong input")
+
+
+
+        elif message.content.startswith(bot_prefix + "delete"): #deletes messages, 50 max in 1 run
+            if(message.author.id == 181439459894624256): #only Sacrier#2869 can delete messages
+                message_number = int(message.content.split()[1])
+                if message_number > 0:
+                    if message_number > 50:
+                        message_number = 50
+                    try:
+                        await message.channel.purge(limit=message_number + 1)
+                        await message.channel.send(f"Deleted {message_number} messages")
+                    except:
+                        await message.channel.send(f"Write a proper integer Ex. {bot_prefix}delete 5 " + message.author.mention, mention_author=True)
+
                     
-        elif message.content.startswith('$restart'): #restarts the bot by creating new process
-            await message.channel.send("Restarting... Wait for 3 seconds.")
+        elif message.content.startswith(bot_prefix + "restart"): #restarts the bot by creating new process
+            await message.channel.send("Restarting... Wait for 3 seconds.") # to make sure bot is connected after restarting
             python = sys.executable
             os.execl(python, python, * sys.argv)
 
-        elif message.content.startswith('$exit'): # exits
-            if(message.author.id == 181439459894624256):
+
+        elif message.content.startswith(bot_prefix + "exit"): # exits
+            if(message.author.id == 181439459894624256): #only Sacrier#2869 can shut down the bot
                 await message.channel.send("Disconnecting...")
                 await client.close()
             else:
-                await message.channel.send("You can't close me " + message.author.mention, mention_author=True)
+                await message.channel.send("You can't shut down me " + message.author.mention, mention_author=True)
 
-        elif message.content.startswith('$help'): # shows help menu
-            text_channel = client.get_channel(594596796890873871)
-            embed = discord.Embed(title = "Commands", description = "Description of the commands")
-            embed.add_field(name = "$dice", value = "Returns random number between 1 and 6")
-            embed.add_field(name = "$aram", value = f'only works in {text_channel.mention}')
-            embed.add_field(name = "$restart", value = "Restarts the bot so $aram works again")
-            embed.add_field(name = "$exit", value = "Disconnects the bot")
-            await message.channel.send(content=None, embed=embed)
 
-        elif message.content.startswith('$dice'): # dice 1to6
-            await message.channel.send(randint(1,6), mention_author=True)
+        elif message.content.startswith(bot_prefix + "dice"): # dice 1to6
+            await message.channel.send(random.randint(1,6)  + " " + message.author.mention, mention_author=True)
 
-        elif message.content.startswith('$aram'): # tries to call people for aram
-            if(message.channel.name == "bot"):
-                aram_role = message.guild.get_role(847982643702267935)
-                if aram:
-                    return
-                aram = 1
-                channel = message.channel
-                playerNumber = 0
-                player_list = []
-                nplayer_list = []
-                emptyPlayer = ""
-                emptynPlayer = ""
-                call_msg = "Takım kuruldu oyuna girin"
-                msg = await channel.send(f"Aram gelen ✅, gelmeyen ❌ {aram_role.mention}")
-                await msg.add_reaction('✅')
-                await msg.add_reaction('❌')
 
-                def check(reaction, user):
-                    return (str(reaction.emoji) == '✅') or (str(reaction.emoji) == '❌')
+        elif message.content.startswith(bot_prefix + "aram"): # tries to call people for aram
+            try:
+                if(message.channel.name in {"aram", "bot", "bottest"}):
 
-                while playerNumber < 5:
-                    reaction, user = await client.wait_for('reaction_add', check=check)
-                    if user != client.user:
-                        if str(reaction.emoji) == '✅':
-                            if user not in player_list and user not in nplayer_list:
-                                playerNumber += 1
-                                player_list.append(user)
-                                await channel.send(f"{user.mention} ✅ {playerNumber}/5")
-                                if playerNumber == 5:
-                                    embed = discord.Embed(title = "Aram")
-                                    for player in player_list:
-                                        emptyPlayer = emptyPlayer + "\n" + str(player)
-                                        call_msg = call_msg + " " + player.mention
-                                    for nplayer in nplayer_list:
-                                        emptynPlayer = emptynPlayer + "\n" + str(nplayer)
-                                    embed.add_field(name = "Gelenler", value = f"{emptyPlayer}")
-                                    embed.add_field(name = "Gelmeyenler", value = f"-{emptynPlayer}")
-                                    await message.channel.send(content=None, embed=embed)
-                                    await channel.send(call_msg)
-                                    aram = 0
-                        elif str(reaction.emoji) == '❌':
-                            if user not in nplayer_list and user not in player_list:
-                                nplayer_list.append(user)
-                                await channel.send(f"{user.mention} ❌")
+                    aram_role = message.guild.get_role(847982643702267935) #gets the id of "Aram" role so it can ping
+
+                    if aram_in_progress:
+                        return
+
+                    aram_in_progress = True
+
+                    player_accepted = 0
+                    if(len(message.content.split()) == 1):
+                        player_needed = 5
+                    else:
+                        player_needed = int(message.content.split()[1])
+
+                    if player_needed in {5, 6, 8, 10}:
+                    
+                        accepted_list = []
+                        declined_list = []
+                        accepted_string = ""
+                        declined_string = ""
+                        team1_string = ""
+                        team2_string = ""
+
+                        call_msg = "Hazırlık bitti, oyuna girin"
+
+                        msg = await message.channel.send(f"Aram gelen ✅, gelmeyen ❌ {player_needed} kişi gerekiyor {aram_role.mention}")
+                        await msg.add_reaction('✅')
+                        await msg.add_reaction('❌')
+
+                        def check(reaction, user):
+                            return (str(reaction.emoji) == '✅') or (str(reaction.emoji) == '❌')
+
+                        while player_accepted < player_needed:
+
+                            reaction, user = await client.wait_for('reaction_add', check=check)
+
+                            if user != client.user:
+
+                                if str(reaction.emoji) == '✅':
+
+                                    if user not in accepted_list and user not in declined_list:
+
+                                        player_accepted += 1
+                                        accepted_list.append(user)
+                                        await message.channel.send(f"{user.mention} ✅ {player_accepted}/{player_needed}")
+                                        if player_accepted == player_needed:
+
+                                            if player_needed == 5:
+
+                                                embed = discord.Embed(title = "Default Aram")
+
+                                                for player in accepted_list:
+                                                    accepted_string = accepted_string + "\n" + str(player)
+                                                    call_msg = call_msg + " " + player.mention
+
+                                                for nplayer in declined_list:
+                                                    declined_string = declined_string + "\n" + str(nplayer)
+
+                                                embed.add_field(name = "Gelenler", value = f"{accepted_string}")
+                                                embed.add_field(name = "Gelmeyenler", value = f"-{declined_string}")
+
+                                            else:
+                                                embed = discord.Embed(title = f"{player_needed/2}v{player_needed/2} Aram")
+                                                random.shuffle(accepted_list)
+
+                                                for player in accepted_list[:player_needed/2]:
+                                                    team1_string = team1_string + "\n" + str(player)
+                                                    call_msg = call_msg + " " + player.mention
+
+                                                for player in accepted_list[player_needed/2:]:
+                                                    team2_string = team2_string + "\n" + str(player)
+                                                    call_msg = call_msg + " " + player.mention
+
+                                                embed.add_field(name = "Team1", value = f"{team1_string}")
+                                                embed.add_field(name = "Team2", value = f"{team2_string}")
+
+                                            message.channel.send(content=None, embed=embed)
+                                            await message.channel.send(call_msg)
+                                            aram_in_progress = False
+
+                                elif str(reaction.emoji) == '❌':
+
+                                    if user not in declined_list and user not in accepted_list:
+                                        declined_list.append(user)
+                                        await message.channel.send(f"{user.mention} ❌")
+                    else:
+                        aram_in_progress = False
+                        await message.channel.send(f"Wrong input. \"{bot_prefix}aram NoOfPlayers\" , only 5-6-8-10 works as numbers. 5 is default.")
+            except:
+                aram_in_progress = False
+                await message.channel.send(f"Wrong input. \"{bot_prefix}aram NoOfPlayers\" , only 5-6-8-10 works as numbers. 5 is default.")
 
 
 
 @client.event 
 async def on_member_update(before, after): # log nickname-role changes
+
     if not before.nick == after.nick:
         with open("logs.txt", "a", encoding="utf-8") as f:
             f.write(f"Time: {int(time.time())}, Date:{datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')},  {before} changed nickname, Before: {before.nick}, After: {after.nick}\n")
+
     else:
         with open("logs.txt", "a", encoding="utf-8") as f:
             f.write(f"Time: {int(time.time())}, Date:{datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')}, {before} changed roles, Before: {before.roles}, After: {after.roles}\n")
 
+
 @client.event
 async def on_user_update(before, after): # log user avatar changes
+
     if not before.avatar == after.avatar:
         with open("logs.txt", "a", encoding="utf-8") as f:
             f.write(f"Time: {int(time.time())}, Date:{datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')},  {before} changed avatar, Before: {before.avatar}, After: {after.avatar}\n")
