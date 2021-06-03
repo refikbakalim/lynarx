3ea8f088660bbb851379750a1e81e2b9859011cb
refikbakalim <refikbakalim@gmail.com>
2021-06-04 00:34:19 +0300

    Update lynarx.py
    Add intents privileges so the members can be logged

--- lynarx.py
+++ lynarx.py
@@ -1,13 +1,16 @@
 from asyncio.windows_events import NULL
 from discord.ext import commands
 import os
+import discord
 
 def read_prefix(): #reads the prefix it saved before
     with open("prefix.txt", "r") as f:
         lines = f.readlines()
         return lines[0].strip()
 
-bot = commands.Bot(command_prefix=read_prefix())
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=read_prefix(), intents=intents)
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
